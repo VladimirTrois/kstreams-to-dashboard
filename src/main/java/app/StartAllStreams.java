15bb5fcbfe44bf43f1148cc9226080c452cb8772
Vladimir TROIS <vladimir.trois@nexworld.fr>
2020-06-09 11:59:44 +0200

    clean package

--- src/main/java/app/StartAllStreams.java
+++ src/main/java/app/StartAllStreams.java
@@ -4,26 +4,28 @@ import model.UserTweetRetweetCount;
 import model.dashboardFormat.UserInfluenceTop30;
 import tools.JsonPOJOSerde;
 
+import java.util.concurrent.TimeUnit;
+
 public class StartAllStreams
 {
     public static void main(String[] args)
     {
-//        try
-//        {
-//            TweetCount1s500msStart.main(args);
-//            TimeUnit.SECONDS.sleep(5);
-//            TweetCount5sStart.main(args);
-//            TimeUnit.SECONDS.sleep(5);
-//            LangCount1h1minStart.main(args);
-//            TimeUnit.SECONDS.sleep(5);
-//            HashtagCount10min10sStart.main(args);
-//            TimeUnit.SECONDS.sleep(5);
-//            UserCount10min10sStart.main(args);
-//            TimeUnit.SECONDS.sleep(5);
-//            TweetMappingStart.main(args);
-//        } catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
+        try
+        {
+            TweetCount1s500msStart.main(args);
+            TimeUnit.SECONDS.sleep(5);
+            TweetCount5sStart.main(args);
+            TimeUnit.SECONDS.sleep(5);
+            LangCount1h1minStart.main(args);
+            TimeUnit.SECONDS.sleep(5);
+            HashtagCount10min10sStart.main(args);
+            TimeUnit.SECONDS.sleep(5);
+            UserCount10min10sStart.main(args);
+            TimeUnit.SECONDS.sleep(5);
+            TweetMappingStart.main(args);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
